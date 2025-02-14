e88cf5e49df295b7dbbc7f5562ffb9347ea486d4
Debjit Dey <debjitdeyofficial6@gmail.com>
2025-02-14 17:07:25 +0530

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,37 @@
-import express from 'express'
-import 'dotenv/config'
-import mongoose from 'mongoose'
-import bookRouter from './routes/book.route.js'
-import userRouter from './routes/user.route.js'
-import cors from 'cors'
-const app = express()
-const port = process.env.PORT || 3001
-app.use(cors())
-app.use(express.json())
-
+import express from "express";
+import "dotenv/config";
+import mongoose from "mongoose";
+import bookRouter from "./routes/book.route.js";
+import userRouter from "./routes/user.route.js";
+import cors from "cors";
+const app = express();
+const port = process.env.PORT || 3001;
+app.use(cors());
+app.use(express.json());
 
 try {
-    const mongoURI = process.env.mongoURI;
-    mongoose.connect(mongoURI);
-    console.log('MongoDB is connected');
+  const mongoURI = process.env.mongoURI;
+  if (!mongoURI) {
+    console.error("MongoDB URI is missing! Check your environment variables.");
+    process.exit(1);
+  }
+  mongoose.connect(mongoURI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("MongoDB is connected");
 } catch (error) {
-    console.log('MongoDB connection unsuccessful');
-    console.log(error);
-    
+  console.log("MongoDB connection unsuccessful");
+  console.log(error);
 }
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.use('/book', bookRouter);
-app.use('/user', userRouter);
+app.use("/book", bookRouter);
+app.use("/user", userRouter);
 
 app.listen(port, () => {
-  console.log(`app listening on port ${port}`)
-})
+  console.log(`app listening on port ${port}`);
+});
